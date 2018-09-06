993be74791c5add1d0cb764738e4dd7e8c0e5e66
Brunno Nogueira <31961873+Sonbroly@users.noreply.github.com>
2018-09-06 14:21:49 -0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  lancamentos = [
+    { tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: '30/06/2017', dataPagamento: null, valor: 4.55, pessoa: 'Padaria do José'
+  }
+  ];
+}
