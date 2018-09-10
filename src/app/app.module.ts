9112d31c607056327cedb42489eb6e4d36a262d1
Brunno <bruno_owned@hotmail.com>
2018-09-09 21:29:48 -0300

    comite

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import {InputTextModule} from 'primeng/inputtext';
-import {ButtonModule} from 'primeng/button';
-import {TableModule} from 'primeng/table';
-
 import { AppComponent } from './app.component';
+import { LancamentosModule } from './lancamentos/lancamentos.module';
+import { PessoasModule } from './pessoas/pessoas.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -13,9 +14,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    InputTextModule,
-    ButtonModule,
-    TableModule
+    BrowserAnimationsModule,
+    FormsModule,
+
+    CoreModule,
+    LancamentosModule,
+    PessoasModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
