fc8da60b6355439468d584efabe2caad0b0da057
Brunno <bruno_owned@hotmail.com>
2018-09-09 22:00:22 -0300

    o

---
+++ src/app/funcionario.service.ts
@@ -0,0 +1,20 @@
+export class FuncionarioService {
+
+  ultimoId = 1;
+  funcionarios = [{ id: 1, nome: 'João' }];
+
+  adicionar(nome) {
+    const funcionario = {
+      id: ++this.ultimoId,
+      nome: nome
+    };
+
+    this.funcionarios.push(funcionario);
+    console.log(funcionario);
+  }
+
+  consultar() {
+    return this.funcionarios;
+  }
+
+}
